f40e07f8bc69d1305ff45f93be67ffaf29650bdf
Victor Duarte <victor.d.dev@gmail.com>
2022-09-12 14:58:09 -0300

    fisrt commit

---
+++ src/server.ts
@@ -0,0 +1,16 @@
+import express from "express";
+
+const app = express();
+
+app.get("/ads", (request, rsponse) => {
+  return rsponse.json([
+    { id: 1, name: "anuncio 1" },
+    { id: 2, name: "anuncio 2" },
+    { id: 3, name: "anuncio 3" },
+    { id: 4, name: "anuncio 4" },
+  ]);
+});
+
+app.listen(3333, () => {
+  console.log("listening on port 3333");
+});
